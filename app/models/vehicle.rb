14d9cd96ad16cd5c56f7df0b7d440ac84516e9bf
Andy Pickler <andy.pickler@gmail.com>
2015-01-18 06:15:28 -0700

    Add validations to vehicle fields

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,10 @@
 class Vehicle < ActiveRecord::Base
+
+  validates_presence_of :vin, :year, :make, :model
+  validates_numericality_of :year, greater_than_or_equal_to: 1900, less_than: 2050
+  validates_length_of :vin, is: 17
+  validates_numericality_of :mileage, greater_than_or_equal_to: 0, less_than: 1_000_000, allow_nil: true
+
 end
 
 # == Schema Information
