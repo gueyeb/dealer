c1082d001acec5cedcbae3b6a598e6ee40e72018
Andy Pickler <andy.pickler@gmail.com>
2015-02-05 12:08:17 -0700

    Update gems and config Rollbar to use SuckerPunch and disable reporting in Dev

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -5,11 +5,7 @@ Rollbar.configure do |config|
 
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
-  # Here we'll disable in 'test':
-  if Rails.env.test?
-    config.enabled = false
-  end
-
+  config.enabled = false unless Rails.env.production?
 
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
@@ -36,14 +32,14 @@ Rollbar.configure do |config|
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
-  # config.use_async = true
+  config.use_async = true
   # Supply your own async handler:
   # config.async_handler = Proc.new { |payload|
   #  Thread.new { Rollbar.process_payload_safely(payload) }
   # }
 
   # Enable asynchronous reporting (using sucker_punch)
-  # config.use_sucker_punch
+  config.use_sucker_punch
 
   # Enable delayed reporting (using Sidekiq)
   # config.use_sidekiq
