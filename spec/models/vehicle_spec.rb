cd11c46b8698b0b913a7e658b7b7d02b124332e5
Andy Pickler <andy.pickler@gmail.com>
2015-02-21 06:58:16 -0700

    Add vehicle validation specs

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,34 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, :type => :model do
-  it "should default 'active' to true" do
-    vehicle = build(:vehicle)
-    expect(vehicle.active).to be_truthy
-  end
-
-  context 'when omitting required fields' do
-    it 'is invalid when VIN is not provided' do
-      vehicle = build(:vehicle, vin: nil)
-      vehicle.valid?
-      expect(vehicle.errors[:vin]).to include("can't be blank")
+  context 'when validating' do
+    it "should default 'active' to true" do
+      vehicle = build(:vehicle)
+      expect(vehicle.active).to be_truthy
     end
 
-    it 'is invalid when year is not provided' do
-      vehicle = build(:vehicle, year: nil)
-      vehicle.valid?
-      expect(vehicle.errors[:year]).to include("can't be blank")
+    [:vin, :year, :make, :model].each do |attr|
+      it "is invalid when #{attr} is not provided" do
+        vehicle = build(:vehicle, attr => nil)
+        vehicle.valid?
+        expect(vehicle.errors[attr]).to include("can't be blank")
+      end
     end
 
-    it 'is invalid when make is not provided' do
-      vehicle = build(:vehicle, make: nil)
-      vehicle.valid?
-      expect(vehicle.errors[:make]).to include("can't be blank")
+    context 'the allowed values of year' do
+      it 'is invalid for years before 1900' do
+        vehicle = build(:vehicle, year: 1899)
+        vehicle.valid?
+        expect(vehicle.errors[:year]).to include('must be greater than or equal to 1900')
+      end
+
+      it 'is invalid for years after 2049' do
+        vehicle = build(:vehicle, year: 2050)
+        vehicle.valid?
+        expect(vehicle.errors[:year]).to include('must be less than 2050')
+      end
     end
 
-    it 'is invalid when model is not provided' do
-      vehicle = build(:vehicle, model: nil)
-      vehicle.valid?
-      expect(vehicle.errors[:model]).to include("can't be blank")
+    context 'the allowed length of vin' do
+      it 'is invalid for less than 17 characters' do
+        vehicle = build(:vehicle, vin: '1234567890123456')
+        vehicle.valid?
+        expect(vehicle.errors[:vin]).to include('is the wrong length (should be 17 characters)')
+      end
+
+      it 'is invalid for more than 17 characters' do
+        vehicle = build(:vehicle, vin: '123456789012345678')
+        vehicle.valid?
+        expect(vehicle.errors[:vin]).to include('is the wrong length (should be 17 characters)')
+      end
+    end
+
+    context 'the allowed mileage values' do
+      it 'is invalid for negative mileage values' do
+        vehicle = build(:vehicle, mileage: -1)
+        vehicle.valid?
+        expect(vehicle.errors[:mileage]).to include('must be greater than or equal to 0')
+      end
+
+      it 'is invalid for mileage values greater than 999_999' do
+        vehicle = build(:vehicle, mileage: 1_000_000)
+        vehicle.valid?
+        expect(vehicle.errors[:mileage]).to include('must be less than 1000000')
+      end
     end
   end
 end
