59723acf0e0cd89b03bfb1690ebc9921f73ce1dc
Andy Pickler <andy.pickler@gmail.com>
2015-03-02 09:11:25 -0700

    Finally figured out limitations of testing authenticated routes

---
+++ spec/views/vehicles/new.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "vehicles/new", type: :view do
+  before(:each) do
+    assign(:vehicle, create(:vehicle))
+  end
+
+  it "renders new vehicle form" do
+    render
+
+    assert_select "form[method=?]", "post" do
+      assert_select "input#vehicle_vin[name=?]", "vehicle[vin]"
+      assert_select "input#vehicle_year[name=?]", "vehicle[year]"
+      assert_select "input#vehicle_make[name=?]", "vehicle[make]"
+      assert_select "input#vehicle_model[name=?]", "vehicle[model]"
+      assert_select "input#vehicle_trim[name=?]", "vehicle[trim]"
+      assert_select "input#vehicle_mileage[name=?]", "vehicle[mileage]"
+      assert_select "input#vehicle_exterior_color[name=?]", "vehicle[exterior_color]"
+      assert_select "input#vehicle_interior_color[name=?]", "vehicle[interior_color]"
+      assert_select "input#vehicle_transmission[name=?]", "vehicle[transmission]"
+      assert_select "input#vehicle_body_style[name=?]", "vehicle[body_style]"
+      assert_select "input#vehicle_drivetrain[name=?]", "vehicle[drivetrain]"
+      assert_select "input#vehicle_engine[name=?]", "vehicle[engine]"
+      assert_select "textarea#vehicle_options[name=?]", "vehicle[options]"
+      assert_select "textarea#vehicle_comments[name=?]", "vehicle[comments]"
+      assert_select "input#vehicle_asking_price[name=?]", "vehicle[asking_price]"
+    end
+  end
+end
---
+++ spec/views/vehicles/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "vehicles/show", type: :view do
+  before(:each) do
+    @vehicle = assign(:vehicle, create(:vehicle))
+  end
+
+  it "renders attributes" do
+    render
+    expect(rendered).to match(/Vin/)
+    expect(rendered).to match(/Exterior Color/)
+    expect(rendered).to match(/Interior Color/)
+    expect(rendered).to match(/Transmission/)
+    expect(rendered).to match(/Body Style/)
+    expect(rendered).to match(/Drivetrain/)
+    expect(rendered).to match(/Engine/)
+  end
+end
