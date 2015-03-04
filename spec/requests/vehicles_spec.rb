e8f218591874b7a49d8a028224beaa51d1b0d0e3
Andy Pickler <andy.pickler@gmail.com>
2015-03-03 17:02:15 -0700

    Okay, so routing, view, and request specs are not really all that useful in this application

--- spec/requests/vehicles_spec.rb
+++ spec/requests/vehicles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Vehicles", type: :request do
   describe "GET /vehicles" do
-    it "works! (now write some real specs)" do
+    it "provides an HTTP 200 status response" do
       get vehicles_path
       expect(response).to have_http_status(200)
     end
