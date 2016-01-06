b6466f889470e372d86a337269e50391664dc277
Andy Pickler <andy.pickler@gmail.com>
2016-01-05 19:59:27 -0700

    Add contact mailer basic testing

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'Contacts', type: :feature do
   scenario 'Filling in the contact form' do
-    visit 'contacts/new'
+    visit '/contacts/new'
+    within('#new_contact') do
+      fill_in 'Name', with: 'John Doe'
+      fill_in 'Email', with: 'john@doe.com'
+      fill_in 'Subject', with: 'Buy a car'
+      fill_in 'Message', with: 'I want to buy your most expensive car'
+    end
+    click_button 'Submit'
+    expect(page).to have_content 'Your message has been sent.'
   end
 end
